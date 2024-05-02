80174407b13a8006321cd34f500fbdf7ef1a1408
Michael Stewart <stewarmc@jmu.edu>
2024-05-02 12:34:28 -0400

    starting point

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [
     legacy({
       // targets: ["defaults", "not IE 11"],
-      targets: "last 5 versions and not dead, > 0.3%, Firefox ESR",
+      // targets: "last 5 versions and not dead, > 0.3%, Firefox ESR",
+      targets: {
+        firefox: "69"
+      },
     }),
   ],
 });
